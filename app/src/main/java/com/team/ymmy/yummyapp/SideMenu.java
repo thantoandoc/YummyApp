df0dc5c81d43d8a3bbcae2435a5d7e36688dafae
Gracefully Broken <thantoandoc83@gmail.com>
2018-09-26 20:30:26 +0700

    side menu

--- app/src/main/java/com/team/ymmy/yummyapp/SideMenu.java
+++ app/src/main/java/com/team/ymmy/yummyapp/SideMenu.java
@@ -2,10 +2,17 @@ package com.team.ymmy.yummyapp;
 
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class SideMenu extends AppCompatActivity {
 
@@ -20,6 +27,23 @@ public class SideMenu extends AppCompatActivity {
         setContentView(R.layout.activity_side_menu);
 
         mapViews();
+        setupToolbar();
+        setupDrawerToogle();
+    }
+
+    private void setupDrawerToogle() {
+//        ActionBarDrawerToggle mDrawerToogle = new ActionBarDrawerToggle(SideMenu.this, mDrawerLayout, mToolbar, R.string.closeDrawer, R.string.openDrawer);
+//        mDrawerLayout.addDrawerListener(mDrawerToogle);
+//        mDrawerToogle.syncState();
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(mToolbar);
+        ActionBar mActionBar =  getSupportActionBar();
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+        mActionBar.setDisplayShowHomeEnabled(false);
+        mActionBar.setDisplayShowTitleEnabled(false);
+        mActionBar.setIcon(R.drawable.ic_arrow_back_24dp);
     }
 
     private void mapViews() {
@@ -27,5 +51,30 @@ public class SideMenu extends AppCompatActivity {
         mToolbar = findViewById(R.id.toolbar);
         mFrameLayout = findViewById(R.id.content_frame);
         mNavigationView = findViewById(R.id.nav_view);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_categories:{
+                if(mDrawerLayout.isDrawerOpen(Gravity.RIGHT)){
+                    mDrawerLayout.closeDrawer(Gravity.RIGHT);
+                }else {
+                    mDrawerLayout.openDrawer(Gravity.RIGHT);
+                }
+                return true;
+            }
+            case android.R.id.home : {
+                onBackPressed();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
