cd1b5dd1b25558d5e8ca7b1cf2fcf5a4ff92722c
Le Cam <thantoandoc83@gmail.com>
2018-09-29 22:58:57 +0700

    login

--- app/src/main/java/com/team/ymmy/yummyapp/LoginActivity.java
+++ app/src/main/java/com/team/ymmy/yummyapp/LoginActivity.java
@@ -1,8 +1,14 @@
 package com.team.ymmy.yummyapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.PatternMatcher;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -10,6 +16,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
@@ -18,18 +29,36 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private Button mLogin;
     private CheckBox mRemember;
     private TextView mForgotPassword;
+    private FirebaseAuth mAuth;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+
+    private static final String USERNAME = "USERNAME";
+    private static final String PASSWORD = "PASSWORD";
+    private static final String CHECKBOX_STATE = "CHECKBOX_STATE";
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(LoginActivity.this, TableActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         mapViews();
-
+        initReferences();
         handleEvents();
-
-
     }
+
+
 
     private void handleEvents() {
         mLogin.setOnClickListener(this);
@@ -43,15 +72,60 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         }
     }
     private void loginEvent() {
-        String vUserName = mUserName.getText().toString();
-        String vPassWord = mPassWord.getText().toString();
-        if(!vUserName.trim().equals("")||!vPassWord.trim().equals("")){
-            Toast.makeText(LoginActivity.this,"input please!", Toast.LENGTH_SHORT).show();
-        }else {
-            Intent intent = new Intent(LoginActivity.this, TableActivity.class);
-            startActivity(intent);
-            finish();
+        String userName = mUserName.getText().toString().trim();
+        String passWord = mPassWord.getText().toString().trim();
+        if(userName.isEmpty()) {
+            mUserName.setError("Email is required!");
+            mUserName.requestFocus();
+            return;
         }
+        if(!Patterns.EMAIL_ADDRESS.matcher(userName).matches()) {
+            mUserName.setError("Email invalid!");
+            mUserName.requestFocus();
+            return;
+        }
+
+        if(passWord.isEmpty()) {
+            mPassWord.setError("Password is required!");
+            mPassWord.requestFocus();
+            return;
+        }
+        if(passWord.length() < 6) {
+            mPassWord.setError("Minimum length of password should be 6!");
+            mPassWord.requestFocus();
+            return;
+        }
+        if(mRemember.isChecked()) {
+            mEditor.putString(USERNAME, userName);
+            mEditor.putString(PASSWORD, passWord);
+            mEditor.putBoolean(CHECKBOX_STATE, mRemember.isChecked());
+            mEditor.commit();
+        }
+        mAuth.signInWithEmailAndPassword(userName, passWord).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    Toast.makeText(LoginActivity.this, "Login success!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, TableActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Login isn't success!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    private void initReferences() {
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(LoginActivity.this);
+        mEditor = mSharedPreferences.edit();
+
+        String username = mSharedPreferences.getString(USERNAME, "");
+        String password = mSharedPreferences.getString(PASSWORD, "");
+        boolean stateCheckBox = mSharedPreferences.getBoolean(CHECKBOX_STATE, false);
+        mUserName.setText(username);
+        mPassWord.setText(password);
+        mRemember.setChecked(stateCheckBox);
     }
     private void mapViews() {
         mUserName=(EditText)findViewById(R.id.edt_username);
@@ -59,6 +133,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mLogin = (Button)findViewById(R.id.btn_login);
         mRemember=(CheckBox)findViewById(R.id.chk_remember);
         mForgotPassword=(TextView)findViewById(R.id.txt_forgot_password);
-    }
 
+        mAuth = FirebaseAuth.getInstance();
+    }
 }
